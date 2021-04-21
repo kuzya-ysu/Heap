6f10edc16816567e324057c29016cc60c5167feb
danya131100 <danya131100@gmail.com>
2021-04-21 23:27:56 +0300

    heap done

--- Heap/BinaryHeap.cs
+++ Heap/BinaryHeap.cs
@@ -10,34 +10,65 @@ namespace Heap
     {
         public int Count { get; set; }
         public int Capacity { get; set; }
-        private T[] Heap { get; set; }
+        private List<T> Heap { get; set; }
         private IComparer<T> Comparer { get; set; }
 
         public BinaryHeap(IComparer<T> comparer)
         {
             Comparer = comparer;
         }
+
         public BinaryHeap(ICollection<T> collection, IComparer<T> comparer)
         {
-            collection.CopyTo(Heap, 0);
-            Count = Heap.Length;
+            Heap = collection.ToList();
+            Count = Heap.Count;
             Capacity = (int)Math.Pow(2, Math.Ceiling(Math.Log(Count, 2)));
             Comparer = comparer;
+            BuildHeap(Heap);
         }
+
         public BinaryHeap(int capacity, IComparer<T> comparer)
         {
             Capacity = capacity;
             Comparer = comparer;
         }
 
+        public void Add(T value)
+        {
+            Heap.Add(value);
+            Count++;
+            int i = Count - 1;
+            int parent = (i - 1) / 2;
 
-        private void BuildHeap(T[] array)
+            while (i > 0 && Comparer.Compare(Heap[parent], Heap[i]) < 0)
+            {
+                var temp = Heap[i];
+                Heap[i] = Heap[parent];
+                Heap[parent] = temp;
+
+                i = parent;
+                parent = (i - 1) / 2;
+            }
+        }
+
+        public T GetMaxOrMin()
+        {
+            return Heap[0];
+        }
+
+        public void RemoveMaxOrMin()
+        {
+            Heap[0] = Heap[Count - 1];
+            Heap.RemoveAt(Count - 1);
+            Heapify(0);
+        }
+
+        private void BuildHeap(List<T> list)
         {
             for (var i = (Count - 1) / 2; i >= 0; i--)
             {
                 Heapify(i);
             }
-
         }
 
         private void Heapify(int index)
@@ -50,15 +81,15 @@ namespace Heap
             if (right < Count && Comparer.Compare(Heap[right], Heap[largest]) > 0)
             { largest = right; }
 
-            if (largest == index) return;
+            if (largest == index)
+                return;
             var temp = Heap[largest];
             Heap[largest] = Heap[index];
             Heap[index] = temp;
             Heapify(largest);
         }
-
-
     }
+
     public class MaxComparer<T> : IComparer<T> where T : IComparable
     {
         public int Compare(T x, T y)
---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Heap;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MaxComparer<int> comparer = new MaxComparer<int>();
+            List<int> list = new List<int> { 8, 15, 11, 6, 9, 7, 8, 1, 3, 5 };
+            BinaryHeap<int> kucha = new BinaryHeap<int>(list, comparer);
+            Console.WriteLine(kucha.GetMaxOrMin());
+            Console.ReadKey();
+        }
+    }
+}
